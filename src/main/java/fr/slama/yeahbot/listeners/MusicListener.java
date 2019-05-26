68dbf8a532b0d4b410934251fc36a5d6b633f09f
Slama <irwin.madet@gmail.com>
2019-05-26 17:34:27 +0200

    Clean out MusicListener

--- src/main/java/fr/slama/yeahbot/listeners/MusicListener.java
+++ src/main/java/fr/slama/yeahbot/listeners/MusicListener.java
@@ -7,6 +7,7 @@ import fr.slama.yeahbot.music.MusicManager;
 import net.dv8tion.jda.core.entities.VoiceChannel;
 import net.dv8tion.jda.core.events.guild.voice.GuildVoiceLeaveEvent;
 import net.dv8tion.jda.core.events.guild.voice.GuildVoiceMoveEvent;
+import net.dv8tion.jda.core.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 /**
@@ -18,33 +19,27 @@ public class MusicListener extends ListenerAdapter {
 
     @Override
     public void onGuildVoiceLeave(GuildVoiceLeaveEvent event) {
-
-        if (!event.getMember().equals(event.getGuild().getSelfMember()) &&
-                event.getChannelLeft().equals(event.getGuild().getSelfMember().getVoiceState().getChannel()) &&
-                event.getChannelLeft().getMembers().stream().filter(m -> !m.getUser().isBot()).toArray().length < 1) {
-            leave(event.getChannelLeft());
-        }
+        check(event);
         super.onGuildVoiceLeave(event);
-
     }
 
     @Override
     public void onGuildVoiceMove(GuildVoiceMoveEvent event) {
-
-        if (!event.getMember().equals(event.getGuild().getSelfMember()) &&
-                event.getChannelLeft().equals(event.getGuild().getSelfMember().getVoiceState().getChannel()) &&
-                event.getChannelLeft().getMembers().stream().filter(m -> !m.getUser().isBot()).toArray().length < 1) {
-            leave(event.getChannelLeft());
-        }
+        check(event);
         super.onGuildVoiceMove(event);
     }
 
-    private void leave(VoiceChannel channel) {
-        channel.getGuild().getAudioManager().closeAudioConnection();
-        manager.getPlayer(channel.getGuild()).getTextChannel().sendMessage(
-                LanguageUtil.getArguedString(channel.getGuild(), Bundle.STRINGS, "alone_in_channel", channel.getName())
-        ).queue();
-        manager.getPlayer(channel.getGuild()).getTrackScheduler().stop();
+    private void check(GuildVoiceUpdateEvent event) {
+        if (!event.getMember().equals(event.getGuild().getSelfMember()) &&
+                event.getChannelLeft().equals(event.getGuild().getSelfMember().getVoiceState().getChannel()) &&
+                event.getChannelLeft().getMembers().stream().filter(m -> !m.getUser().isBot()).toArray().length < 1) {
+            VoiceChannel channel = event.getChannelLeft();
+            channel.getGuild().getAudioManager().closeAudioConnection();
+            manager.getPlayer(channel.getGuild()).getTextChannel().sendMessage(
+                    LanguageUtil.getArguedString(channel.getGuild(), Bundle.STRINGS, "alone_in_channel", channel.getName())
+            ).queue();
+            manager.getPlayer(channel.getGuild()).getTrackScheduler().stop();
+        }
     }
 
 }
