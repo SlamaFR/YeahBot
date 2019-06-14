f574ae75f4d4f7b78b6c31acd58938cdeb812046
Slama <irwin.madet@gmail.com>
2019-06-14 17:35:10 +0200

    Rename variable

--- src/main/java/fr/slama/yeahbot/utilities/MessageUtil.java
+++ src/main/java/fr/slama/yeahbot/utilities/MessageUtil.java
@@ -1,7 +1,6 @@
 package fr.slama.yeahbot.utilities;
 
 import fr.slama.yeahbot.language.Bundle;
-import fr.slama.yeahbot.language.LanguageUtil;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Guild;
@@ -12,17 +11,17 @@ import net.dv8tion.jda.core.entities.TextChannel;
  */
 public class MessageUtil {
 
-    public static void sendPermissionEmbed(Guild guild, TextChannel textChannel, Permission... permission) {
+    public static void sendPermissionEmbed(Guild guild, TextChannel textChannel, Permission... permissions) {
         String perm;
-        String title_key = permission.length < 2 ? "permission_needed" : "permissions_needed";
-        String key = permission.length < 2 ? "need_permission" : "need_permissions";
+        String title_key = permissions.length < 2 ? "permission_needed" : "permissions_needed";
+        String key = permissions.length < 2 ? "need_permission" : "need_permissions";
 
-        if (permission.length < 2) {
-            perm = LanguageUtil.getString(guild, Bundle.PERMISSION, permission[0].toString().toLowerCase());
+        if (permissions.length < 2) {
+            perm = LanguageUtil.getString(guild, Bundle.PERMISSION, permissions[0].toString().toLowerCase());
         } else {
             StringBuilder builder = new StringBuilder();
 
-            for (Permission p : permission) {
+            for (Permission p : permissions) {
                 if (builder.length() > 1) builder.append(", ");
                 builder.append(LanguageUtil.getString(guild, Bundle.PERMISSION, p.toString().toLowerCase()));
             }
