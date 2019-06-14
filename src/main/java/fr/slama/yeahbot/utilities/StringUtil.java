4b85102bfcc874dfc9d57030375e3b7a2c504251
Slama <irwin.madet@gmail.com>
2019-06-14 17:37:14 +0200

    Add StringUtil

---
+++ src/main/java/fr/slama/yeahbot/utilities/StringUtil.java
@@ -0,0 +1,21 @@
+package fr.slama.yeahbot.utilities;
+
+/**
+ * Created on 2019-06-14.
+ */
+public class StringUtil {
+
+    public static String replaceLast(String find, String replace, String string) {
+        int lastIndex = string.lastIndexOf(find);
+
+        if (lastIndex == -1) {
+            return string;
+        }
+
+        String beginString = string.substring(0, lastIndex);
+        String endString = string.substring(lastIndex + find.length());
+
+        return beginString + replace + endString;
+    }
+
+}
