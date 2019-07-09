416f9f4f32e001d0e1a622217180c8e28903c1b1
Slama <irwin.madet@gmail.com>
2019-07-10 00:00:18 +0200

    Add an auto-pause system when everyone is deaf

--- src/main/java/fr/slama/yeahbot/listeners/MusicListener.java
+++ src/main/java/fr/slama/yeahbot/listeners/MusicListener.java
@@ -1,17 +1,23 @@
 package fr.slama.yeahbot.listeners;
 
 import fr.slama.yeahbot.YeahBot;
+import fr.slama.yeahbot.blub.TaskScheduler;
 import fr.slama.yeahbot.language.Bundle;
 import fr.slama.yeahbot.managers.MusicManager;
 import fr.slama.yeahbot.utilities.ColorUtil;
 import fr.slama.yeahbot.utilities.LanguageUtil;
 import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.VoiceChannel;
-import net.dv8tion.jda.core.events.guild.voice.GuildVoiceLeaveEvent;
-import net.dv8tion.jda.core.events.guild.voice.GuildVoiceMoveEvent;
-import net.dv8tion.jda.core.events.guild.voice.GuildVoiceUpdateEvent;
+import net.dv8tion.jda.core.events.guild.voice.*;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created on 22/09/2018.
@@ -19,17 +25,43 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter;
 public class MusicListener extends ListenerAdapter {
 
     private final MusicManager manager = YeahBot.getInstance().getMusicManager();
+    private final Map<Guild, TaskScheduler> tasks = new HashMap<>();
 
     @Override
     public void onGuildVoiceLeave(GuildVoiceLeaveEvent event) {
         check(event);
+        checkDeafen(event);
         super.onGuildVoiceLeave(event);
     }
 
     @Override
     public void onGuildVoiceMove(GuildVoiceMoveEvent event) {
         check(event);
+        checkDeafen(event);
         super.onGuildVoiceMove(event);
+    }
+
+    @Override
+    public void onGuildVoiceDeafen(GuildVoiceDeafenEvent event) {
+        checkDeafen(event);
+        super.onGuildVoiceDeafen(event);
+    }
+
+    private void checkDeafen(GenericGuildVoiceEvent event) {
+        Guild guild = event.getGuild();
+        VoiceChannel channel = event.getVoiceState().getChannel();
+
+        if (!event.getGuild().getAudioManager().isConnected()) return;
+        if (channel != event.getGuild().getSelfMember().getVoiceState().getChannel()) return;
+
+        List<Member> members = channel.getMembers().stream().filter(m -> !m.getUser().isBot()).collect(Collectors.toList());
+        List<Member> deafMembers = members.stream().filter(m -> m.getVoiceState().isDeafened()).collect(Collectors.toList());
+
+        if (deafMembers.size() < members.size()) {
+            resume(guild);
+        } else {
+            pause(guild);
+        }
     }
 
     private void check(GuildVoiceUpdateEvent event) {
@@ -38,9 +70,13 @@ public class MusicListener extends ListenerAdapter {
 
         if (!event.getGuild().getAudioManager().isConnected()) return;
 
-        if (event.getMember().getUser().getIdLong() == event.getJDA().getSelfUser().getIdLong() ||
-                (event.getChannelLeft().equals(event.getGuild().getSelfMember().getVoiceState().getChannel()) &&
-                        event.getChannelLeft().getMembers().stream().filter(m -> !m.getUser().isBot()).toArray().length < 1)) {
+        List<Member> members = channel.getMembers().stream().filter(m -> !m.getUser().isBot()).collect(Collectors.toList());
+
+        boolean botLeft = event.getMember().getUser().getIdLong() == event.getJDA().getSelfUser().getIdLong();
+        boolean inCurrentChannel = event.getChannelLeft().equals(event.getGuild().getSelfMember().getVoiceState().getChannel());
+        boolean botAlone = members.isEmpty();
+
+        if (botLeft || (botAlone && inCurrentChannel)) {
             textChannel.sendMessage(
                     new EmbedBuilder()
                             .setTitle(LanguageUtil.getString(channel.getGuild(), Bundle.CAPTION, "audio_player"))
@@ -50,5 +86,16 @@ public class MusicListener extends ListenerAdapter {
             ).queue();
             manager.getPlayer(channel.getGuild()).getTrackScheduler().stop();
         }
+    }
+
+    private void pause(Guild guild) {
+        if (tasks.containsKey(guild)) tasks.remove(guild).stop();
+        manager.getPlayer(guild).pause(false);
+    }
+
+    private void resume(Guild guild) {
+        tasks.put(guild, TaskScheduler.scheduleDelayed(() -> {
+            manager.getPlayer(guild).resume(false);
+        }, 1000));
     }
 }
