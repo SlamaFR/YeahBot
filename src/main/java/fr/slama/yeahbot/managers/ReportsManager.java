608da6ece7a9a818a7de92b90970f7bf99fff62f
Slama <irwin.madet@gmail.com>
2019-07-21 21:45:09 +0200

    Summarize ReportsManager with functions

--- src/main/java/fr/slama/yeahbot/managers/ReportsManager.java
+++ src/main/java/fr/slama/yeahbot/managers/ReportsManager.java
@@ -1,5 +1,6 @@
 package fr.slama.yeahbot.managers;
 
+import fr.slama.yeahbot.blub.Sanction;
 import fr.slama.yeahbot.blub.SpamType;
 import fr.slama.yeahbot.commands.core.Command;
 import fr.slama.yeahbot.language.Bundle;
@@ -16,13 +17,15 @@ import net.dv8tion.jda.core.entities.TextChannel;
 
 import java.time.Instant;
 import java.util.Collections;
+import java.util.Map;
 
 /**
  * Created on 28/09/2018.
  */
 public class ReportsManager {
 
-    public static void reportSpam(Message message, TextChannel textChannel, SpamType type) {
+    public static void reportSpam(Message message, SpamType type) {
+        TextChannel textChannel = message.getTextChannel();
         Member author = message.getMember();
         long memberId = author.getUser().getIdLong();
 
@@ -31,48 +34,25 @@ public class ReportsManager {
 
         Settings settings = RedisData.getSettings(author.getGuild());
 
-        EmbedBuilder embedBuilder = new EmbedBuilder()
-                .setColor(ColorUtil.ORANGE)
-                .setTitle(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "report_spam"))
-                .addField(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "user"), author.getAsMention(), true)
-                .addField(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "text_channel"), textChannel.getAsMention(), true)
-                .addField(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "report_amount"), String.format("**`%d`**", reports.getSpamReports().get(memberId)), true)
-                .addField(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "type"), LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, type.toKey()), true)
-                .addField(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "jump_to_message"),
-                        (textChannel.getMessageById(message.getId()) != null
-                                ? LanguageUtil.getLink(textChannel.getGuild(), message.getJumpUrl())
-                                : String.format("`%s`", LanguageUtil.getString(textChannel.getGuild(), Bundle.ERROR, "message_unavailable"))
-                        ), true)
-                .setTimestamp(Instant.now());
-
+        EmbedBuilder embedBuilder = getEmbed(message, "report_spam")
+                .addField(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "report_amount"),
+                        String.format("**`%d`**", reports.getSpamReports().get(memberId)), true)
+                .addField(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "type"),
+                        LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, type.toKey()), true);
         String reason = LanguageUtil.getString(author.getGuild(), Bundle.CAPTION, "reason_spam");
-        boolean applied = false;
 
         String warning = type.getSentenceFromSettings(settings);
         if (warning.isEmpty())
             warning = LanguageUtil.getString(textChannel.getGuild(), Bundle.STRINGS, type.toWarningKey());
 
-        if (settings.spamPolicy.containsKey(reports.getSpamReports().get(memberId))) {
-            applied = true;
-            settings.spamPolicy.get(reports.getSpamReports().get(memberId)).apply(textChannel, author, reason);
-        } else if (reports.getSpamReports().get(memberId) > Collections.max(settings.spamPolicy.keySet())) {
-            applied = true;
-            settings.spamPolicy.get(Collections.max(settings.spamPolicy.keySet())).apply(textChannel, author, reason);
-        } else {
-            textChannel.sendMessage(new EmbedBuilder()
-                    .setColor(ColorUtil.ORANGE)
-                    .setAuthor(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "warning"), null, author.getUser().getAvatarUrl())
-                    .setDescription(warning.replace("$user", author.getAsMention()))
-                    .build()).queue();
-        }
-
-        if (applied && (Command.CommandPermission.STAFF.test(author) || !author.getGuild().getSelfMember().canInteract(author)))
+        if (applySanction(reports, settings.spamPolicy, message, warning, reason) && cannotInteract(author))
             embedBuilder.setFooter(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "sanction_non_applied"), null);
 
         notify(author, embedBuilder, reports);
     }
 
-    public static void reportSwearing(Message message, TextChannel textChannel) {
+    public static void reportSwearing(Message message) {
+        TextChannel textChannel = message.getTextChannel();
         Member author = message.getMember();
         long memberId = author.getUser().getIdLong();
 
@@ -81,48 +61,24 @@ public class ReportsManager {
 
         Settings settings = RedisData.getSettings(textChannel.getGuild());
 
-        EmbedBuilder embedBuilder = new EmbedBuilder()
-                .setColor(ColorUtil.ORANGE)
-                .setTitle(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "report_swearing"))
-                .addField(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "user"), author.getAsMention(), true)
-                .addField(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "text_channel"), textChannel.getAsMention(), true)
-                .addField(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "report_amount"), String.format("**`%d`**", reports.getSwearingReports().get(memberId)), true)
-                .addField(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "jump_to_message"),
-                        (textChannel.getMessageById(message.getId()) != null
-                                ? LanguageUtil.getLink(textChannel.getGuild(), message.getJumpUrl())
-                                : String.format("`%s`", LanguageUtil.getString(textChannel.getGuild(), Bundle.ERROR, "message_unavailable"))
-                        ), true)
-                .setTimestamp(Instant.now());
-
+        EmbedBuilder embedBuilder = getEmbed(message, "report_swearing")
+                .addField(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "report_amount"),
+                        String.format("**`%d`**", reports.getSwearingReports().get(memberId)), true);
         String reason = LanguageUtil.getString(author.getGuild(), Bundle.CAPTION, "reason_swearing");
-        boolean applied = false;
 
         String warning = settings.swearingWarningSentence;
         if (warning.isEmpty())
             warning = LanguageUtil.getString(textChannel.getGuild(), Bundle.STRINGS, "swearing_warning_sentence");
 
-        if (settings.swearingPolicy.containsKey(reports.getSwearingReports().get(memberId))) {
-            applied = true;
-            settings.swearingPolicy.get(reports.getSwearingReports().get(memberId)).apply(textChannel, author, reason);
-        } else if (reports.getSwearingReports().get(memberId) > Collections.max(settings.swearingPolicy.keySet())) {
-            applied = true;
-            settings.swearingPolicy.get(Collections.max(settings.swearingPolicy.keySet())).apply(textChannel, author, reason);
-        } else {
-            textChannel.sendMessage(new EmbedBuilder()
-                    .setColor(ColorUtil.ORANGE)
-                    .setAuthor(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "warning"), null, author.getUser().getAvatarUrl())
-                    .setDescription(warning.replace("$user", author.getAsMention()))
-                    .build()).queue();
-        }
-
-        if (applied && (Command.CommandPermission.STAFF.test(author) || !author.getGuild().getSelfMember().canInteract(author)))
+        if (applySanction(reports, settings.swearingPolicy, message, warning, reason) && cannotInteract(author))
             embedBuilder.setFooter(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "sanction_non_applied"), null);
 
         notify(author, embedBuilder, reports);
     }
 
-    public static void reportAdvertising(Message message, TextChannel textChannel) {
-        Member author = message.getGuild().getMember(message.getAuthor());
+    public static void reportAdvertising(Message message) {
+        TextChannel textChannel = message.getTextChannel();
+        Member author = message.getMember();
         long memberId = author.getUser().getIdLong();
 
         Reports reports = RedisData.getReports(textChannel.getGuild());
@@ -130,44 +86,60 @@ public class ReportsManager {
 
         Settings settings = RedisData.getSettings(textChannel.getGuild());
 
-        EmbedBuilder embedBuilder = new EmbedBuilder()
+        EmbedBuilder embedBuilder = getEmbed(message, "report_advertising")
+                .addField(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "report_amount"),
+                        String.format("**`%d`**", reports.getAdvertisingReports().get(memberId)), true);
+        String reason = LanguageUtil.getString(author.getGuild(), Bundle.CAPTION, "reason_advertising");
+
+        String warning = settings.advertisingWarningSentence;
+        if (warning.isEmpty())
+            warning = LanguageUtil.getString(textChannel.getGuild(), Bundle.STRINGS, "advertising_warning_sentence");
+
+        if (applySanction(reports, settings.advertisingPolicy, message, warning, reason) && cannotInteract(author))
+            embedBuilder.setFooter(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "sanction_non_applied"), null);
+
+        notify(author, embedBuilder, reports);
+    }
+
+    private static EmbedBuilder getEmbed(Message message, String type) {
+        TextChannel textChannel = message.getTextChannel();
+        Member author = message.getMember();
+
+        return new EmbedBuilder()
                 .setColor(ColorUtil.ORANGE)
-                .setTitle(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "report_advertising"))
+                .setTitle(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, type))
                 .addField(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "user"), author.getAsMention(), true)
                 .addField(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "text_channel"), textChannel.getAsMention(), true)
-                .addField(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "report_amount"), String.format("**`%d`**", reports.getAdvertisingReports().get(memberId)), true)
                 .addField(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "jump_to_message"),
                         (textChannel.getMessageById(message.getId()) != null
                                 ? LanguageUtil.getLink(textChannel.getGuild(), message.getJumpUrl())
                                 : String.format("`%s`", LanguageUtil.getString(textChannel.getGuild(), Bundle.ERROR, "message_unavailable"))
                         ), true)
                 .setTimestamp(Instant.now());
+    }
 
-        String reason = LanguageUtil.getString(author.getGuild(), Bundle.CAPTION, "reason_advertising");
+    private static boolean applySanction(Reports reports, Map<Integer, Sanction> policy, Message message, String warning, String reason) {
+        TextChannel textChannel = message.getTextChannel();
+        Member author = message.getMember();
+        long memberId = author.getUser().getIdLong();
+
         boolean applied = false;
 
-        String warning = settings.advertisingWarningSentence;
-        if (warning.isEmpty())
-            warning = LanguageUtil.getString(textChannel.getGuild(), Bundle.STRINGS, "advertising_warning_sentence");
-
-        if (settings.advertisingPolicy.containsKey(reports.getAdvertisingReports().get(memberId))) {
+        if (policy.containsKey(reports.getSpamReports().get(memberId))) {
             applied = true;
-            settings.advertisingPolicy.get(reports.getAdvertisingReports().get(memberId)).apply(textChannel, author, reason);
-        } else if (reports.getAdvertisingReports().get(memberId) > Collections.max(settings.advertisingPolicy.keySet())) {
+            policy.get(reports.getSpamReports().get(memberId)).apply(textChannel, author, reason);
+        } else if (reports.getSpamReports().get(memberId) > Collections.max(policy.keySet())) {
             applied = true;
-            settings.advertisingPolicy.get(Collections.max(settings.advertisingPolicy.keySet())).apply(textChannel, author, reason);
+            policy.get(Collections.max(policy.keySet())).apply(textChannel, author, reason);
         } else {
             textChannel.sendMessage(new EmbedBuilder()
                     .setColor(ColorUtil.ORANGE)
-                    .setAuthor(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "warning"), null, author.getUser().getAvatarUrl())
+                    .setAuthor(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "warning"), null,
+                            author.getUser().getAvatarUrl())
                     .setDescription(warning.replace("$user", author.getAsMention()))
                     .build()).queue();
         }
-
-        if (applied && (Command.CommandPermission.STAFF.test(author) || !author.getGuild().getSelfMember().canInteract(author)))
-            embedBuilder.setFooter(LanguageUtil.getString(textChannel.getGuild(), Bundle.CAPTION, "sanction_non_applied"), null);
-
-        notify(author, embedBuilder, reports);
+        return applied;
     }
 
     private static void notify(Member author, EmbedBuilder embedBuilder, Reports reports) {
@@ -175,6 +147,10 @@ public class ReportsManager {
 
         RedisData.setReports(author.getGuild(), reports);
         if (logChannel != null) logChannel.sendMessage(embedBuilder.build()).queue();
+    }
+
+    private static boolean cannotInteract(Member member) {
+        return Command.CommandPermission.STAFF.test(member) || !member.getGuild().getSelfMember().canInteract(member);
     }
 
 }
